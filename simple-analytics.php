6bad058c10949e1a2226eb7e8428005c116acce2
Adriaan van Rossum <git@adriaan.io>
2019-05-09 17:43:53 +0200

    Skip visits from admins and editors

--- simple-analytics.php
+++ simple-analytics.php
@@ -1,27 +1,39 @@
 <?php
 /*
  * Plugin Name: Simple Analytics
- * Version: 1.0
- * Plugin URI: https://simpleanalytics.com/
+ * Version: 1.1
+ * Plugin URI: https://docs.simpleanalytics.com/install-simple-analytics-on-wordpress
  * Description: Embed Simple Analytics script in your WordPress website
- * Author: Adriaan van Rossum
+ * Author: Simple Analytics
  * Author URI: https://simpleanalytics.com/
- * Requires at least: 1.0
+ * Requires at least: 2.0.0
  * Tested up to: 5.2
  *
  * Text Domain: simple-analytics
  * Domain Path: /lang/
  *
  * @package WordPress
- * @author Adriaan van Rossum
+ * @author Simple Analytics
  * @since 1.0.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-function inject_header_simple_analytics() { echo '<script async src="https://cdn.simpleanalytics.io/hello.js"></script>'; }
+function inject_header_simple_analytics() {
+  if (current_user_can('editor') || current_user_can('administrator')) {
+    echo '<script>console.warn(\'Simple Analytics: Not logging requests from admins.\')</script>';
+  } else {
+    echo '<script async src="https://cdn.simpleanalytics.io/hello.js"></script>';
+  }
+}
 
-function inject_footer_simple_analytics() { echo '<noscript><img src="https://api.simpleanalytics.io/hello.gif" alt=""></noscript>'; }
+function inject_footer_simple_analytics() {
+  if (current_user_can('editor') || current_user_can('administrator')) {
+    // Nothing...
+  } else {
+    echo '<noscript><img src="https://api.simpleanalytics.io/hello.gif" alt=""></noscript>';
+  }
+}
 
 add_action('wp_head', 'inject_header_simple_analytics', 10);
 
