7c788e5fd1a05f9ffabccc6c0f635dfda759fe7c
Adriaan van Rossum <git@adriaan.io>
2019-05-28 14:27:22 +0200

    Use wp_enqueue_script, init, and global $is_running var

--- simple-analytics.php
+++ simple-analytics.php
@@ -1,7 +1,7 @@
 <?php
 /*
- * Plugin Name: Simple Analytics Official Plugin
- * Version: 1.2
+ * Plugin Name: Simple Analytics Official
+ * Version: 1.3
  * Plugin URI: https://docs.simpleanalytics.com/install-simple-analytics-on-wordpress
  * Description: Embed Simple Analytics script in your WordPress website
  * Author: Simple Analytics
@@ -17,24 +17,31 @@
  * @since 1.0.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) exit;
+if (!defined('ABSPATH')) exit;
 
-function inject_header_simple_analytics() {
-  if (current_user_can('editor') || current_user_can('administrator')) {
-    echo '<script>console.warn(\'Simple Analytics: Not logging requests from admins.\')</script>';
-  } else {
-    echo '<script async src="https://cdn.simpleanalytics.io/hello.js"></script>';
+function simpleanalytics_warn_not_logging() {
+  global $is_running;
+  if (!$is_running) {
+    echo '<script>console.warn(\'Simple Analytics: Not logging requests from admins\')</script>';
   }
 }
 
-function inject_footer_simple_analytics() {
-  if (current_user_can('editor') || current_user_can('administrator')) {
-    // Nothing...
-  } else {
+function simpleanalytics_inject_footer() {
+  global $is_running;
+  if ($is_running) {
     echo '<noscript><img src="https://api.simpleanalytics.io/hello.gif" alt=""></noscript>';
   }
 }
 
-add_action('wp_head', 'inject_header_simple_analytics', 10);
+function simpleanalytics_init() {
+  global $is_running;
+  $is_running = !current_user_can('editor') && !current_user_can('administrator');
+  if ($is_running) {
+    wp_enqueue_script('simpleanalytics_script', 'https://cdn.simpleanalytics.io/hello.js', array(), null, true);
+    add_action('wp_footer', 'simpleanalytics_inject_footer', 10);
+  } else {
+    add_action('wp_head', 'simpleanalytics_warn_not_logging', 10);
+  }
+}
 
-add_action('wp_footer', 'inject_footer_simple_analytics', 10);
+add_action('init', 'simpleanalytics_init');
