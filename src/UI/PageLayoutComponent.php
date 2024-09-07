9468d012e03aa35fa7687cc8de00f44b276f8ae2
Ostap Brehin <osbre@protonmail.com>
2024-09-07 16:27:18 +0100

    wip

--- src/UI/PageLayoutComponent.php
+++ src/UI/PageLayoutComponent.php
@@ -34,18 +34,37 @@ readonly class PageLayoutComponent
                 <!-- Header / Nav -->
                 <header class="pt-5 bg-primaryBg">
                     <div class="mx-auto max-w-3xl flex-col justify-between gap-5 sm:flex sm:items-baseline">
-                        <!-- Logo -->
-                        <a
-                            href="https://dashboard.simpleanalytics.com/websites"
-                            target="_blank"
-                            class="text-base font-semibold leading-6 text-gray-900"
-                        >
-                            <img
-                                alt="SimpleAnalytics logo"
-                                src="<?php echo PLUGIN_URL ?>logo.svg"
-                                class="mr-2 inline-block h-10 w-auto text-primary"
+                        <div class="flex items-center">
+                            <!-- Logo -->
+                            <a
+                                href="https://dashboard.simpleanalytics.com/websites"
+                                target="_blank"
+                                class="text-base font-semibold leading-6 text-gray-900"
                             >
-                        </a>
+                                <img
+                                    alt="SimpleAnalytics logo"
+                                    src="<?php echo PLUGIN_URL ?>logo.svg"
+                                    class="mr-2 inline-block h-10 w-auto text-primary"
+                                >
+                            </a>
+                            <!-- "Open Dashboard" link -->
+                            <a
+                                href="https://dashboard.simpleanalytics.com/websites"
+                                target="_blank"
+                                class="inline-flex items-center rounded bg-white px-2 py-1 text-xs font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                            >
+                                <svg class="w-3.5 h-3.5 mr-0.5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                                     fill="currentColor">
+                                    <path fill-rule="evenodd"
+                                          d="M4.25 5.5a.75.75 0 0 0-.75.75v8.5c0 .414.336.75.75.75h8.5a.75.75 0 0 0 .75-.75v-4a.75.75 0 0 1 1.5 0v4A2.25 2.25 0 0 1 12.75 17h-8.5A2.25 2.25 0 0 1 2 14.75v-8.5A2.25 2.25 0 0 1 4.25 4h5a.75.75 0 0 1 0 1.5h-5Z"
+                                          clip-rule="evenodd"/>
+                                    <path fill-rule="evenodd"
+                                          d="M6.194 12.753a.75.75 0 0 0 1.06.053L16.5 4.44v2.81a.75.75 0 0 0 1.5 0v-4.5a.75.75 0 0 0-.75-.75h-4.5a.75.75 0 0 0 0 1.5h2.553l-9.056 8.194a.75.75 0 0 0-.053 1.06Z"
+                                          clip-rule="evenodd"/>
+                                </svg>
+                                Open Dashboard
+                            </a>
+                        </div>
 
                         <!-- Tabs -->
                         <div class="mt-4 sm:mt-0">
