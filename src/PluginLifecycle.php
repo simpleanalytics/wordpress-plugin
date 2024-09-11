389ef8bccdd9ab61535ad377f3aa106246234a57
Ostap Brehin <osbre@protonmail.com>
2024-09-11 21:51:38 +0100

    wip

--- src/PluginLifecycle.php
+++ src/PluginLifecycle.php
@@ -9,14 +9,18 @@ trait PluginLifecycle
         $this->onBoot();
         add_action('init', $this->onInit(...));
         register_activation_hook(ENTRYPOINT_FILE, $this->onActivation(...));
-        register_deactivation_hook(ENTRYPOINT_FILE, $this->onDeactivation(...));
+        register_deactivation_hook(ENTRYPOINT_FILE, $this->onUninstall(...));
     }
 
+    /** @internal */
     abstract protected function onBoot(): void;
 
+    /** @internal */
     abstract public function onInit(): void;
 
+    /** @internal */
     abstract public function onActivation(): void;
 
+    /** @internal */
     abstract public function onUninstall(): void;
 }
