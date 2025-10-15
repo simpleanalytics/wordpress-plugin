95244acc0a80b3e14945428402581bf703b21522
Ostap Brehin <osbre@protonmail.com>
2025-10-15 22:15:09 +0100

    wip

--- tests/Browser/PluginSettingsTest.php
+++ tests/Browser/PluginSettingsTest.php
@@ -6,6 +6,8 @@ test('Sign in', function () {
     $page->fill('user_login', 'admin');
     $page->fill('user_pass', 'admin');
     $page->press('wp-submit');
-
     $page->assertSee('Welcome to WordPress!');
+
+    $page2 = visit('http://127.0.0.1:8100')->screenshot();
+    expect($page2->content())->toContain('<!-- Simple Analytics: Not logging requests from admins -->');
 });
