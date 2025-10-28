a86a7cbbb018169171e4e83d5ea605efb7569448
Ostap Brehin <osbre@protonmail.com>
2025-10-28 11:39:53 +0000

    wip

--- tests/Browser/PluginSettingsTest.php
+++ tests/Browser/PluginSettingsTest.php
@@ -9,7 +9,7 @@ const DEFAULT_SA_NOTICE = '<!-- Simple Analytics: Not logging requests from admi
 test('Sign in as admin and activate the plugin', function () {
     $page = asAdmin();
 
-    $page->navigate('http://localhost:8100/wp-admin/plugins.php')->screenshot();
+    $page->navigate('http://127.0.0.1:8100/wp-admin/plugins.php')->screenshot();
     $page->assertSee('Simple Analytics Official');
 
     $page2 = visit('http://127.0.0.1:8100');//->screenshot();
