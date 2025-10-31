9588940893b62e6391af8780e9819d2896d85644
Ostap Brehin <osbre@protonmail.com>
2025-10-31 17:16:43 +0000

    wip

--- tests/Browser/PluginSettingsTest.php
+++ tests/Browser/PluginSettingsTest.php
@@ -4,8 +4,8 @@ namespace Tests\Browser;
 
 use function Tests\{asAdmin, asAuthor, asEditor};
 
-const SA_ADMIN_NOTICE = '<!-- Simple Analytics: Not logging requests from admins -->';
 const SA_DEFAULT_SCRIPT = 'src="https://scripts.simpleanalyticscdn.com/latest.js"></script>';
+const SA_INACTIVE_ADMIN_NOTICE = '<!-- Simple Analytics: Not logging requests from admins -->';
 const SA_INACTIVE_ADMIN_SCRIPT = 'src="http://localhost:8100/wp-content/plugins/simpleanalytics/resources/js/inactive.js"';
 
 it('can be activated', function () {
@@ -27,7 +27,7 @@ it('adds inactive script for authenticated users by default', function () {
     $homePage = asAdmin()->navigate('http://localhost:8100');
 
     expect($homePage->content())
-        ->toContain(SA_ADMIN_NOTICE)
+        ->toContain(SA_INACTIVE_ADMIN_NOTICE)
         ->toContain(SA_INACTIVE_ADMIN_SCRIPT);
 });
 
@@ -54,19 +54,23 @@ it('adds a script with ignored pages', function () {
 });
 
 it('adds inactive script for selected user roles', function () {
-    asAdmin()
-        ->navigate('http://localhost:8100/wp-admin/options-general.php?page=simpleanalytics&tab=ignore-rules')
+    $admin = asAdmin();
+
+    $admin->navigate('http://localhost:8100/wp-admin/options-general.php?page=simpleanalytics&tab=ignore-rules')
         ->check('simpleanalytics_exclude_user_roles-editor')
         ->check('simpleanalytics_exclude_user_roles-author')
         ->click('Save Changes')
         ->assertChecked('simpleanalytics_exclude_user_roles-editor')
         ->assertChecked('simpleanalytics_exclude_user_roles-author');
 
+    expect($admin->navigate('http://localhost:8100')->content())
+        ->toContain(SA_DEFAULT_SCRIPT);
+
     expect(asEditor()->navigate('http://localhost:8100')->content())
-        ->toContain(SA_ADMIN_NOTICE)
+        ->toContain(SA_INACTIVE_ADMIN_NOTICE)
         ->toContain(SA_INACTIVE_ADMIN_SCRIPT);
 
     expect(asAuthor()->navigate('http://localhost:8100')->content())
-        ->toContain(SA_ADMIN_NOTICE)
+        ->toContain(SA_INACTIVE_ADMIN_NOTICE)
         ->toContain(SA_INACTIVE_ADMIN_SCRIPT);
 });
