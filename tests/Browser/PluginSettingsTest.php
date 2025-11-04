afd5b271475913166d012424db80ac283863c07f
Ostap Brehin <osbre@protonmail.com>
2025-11-04 11:57:14 +0000

    wip

--- tests/Browser/PluginSettingsTest.php
+++ tests/Browser/PluginSettingsTest.php
@@ -6,15 +6,15 @@ class PluginSettingsTest extends BrowserTestCase
 {
     public function test_activation_and_presence_of_default_scripts(): void
     {
-        $this->asAdmin()->visit('http://localhost:8100/wp-admin/plugins.php')
+        $this->asAdmin()->visit('/wp-admin/plugins.php')
             ->click('#activate-simpleanalytics')
             ->assertSeeElement('#deactivate-simpleanalytics')
-            ->visit('http://localhost:8100')
+            ->visit('/')
             ->assertContains('<!-- Simple Analytics: Not logging requests from admins -->')
             ->assertContains('<script src="http://localhost:8100/wp-content/plugins/simpleanalytics/resources/js/inactive.js"');
 
         $this->myBrowser()
-            ->visit('http://localhost:8100')
+            ->visit('/')
             ->assertContains('<script src="https://scripts.simpleanalyticscdn.com/latest.js"></script>');
     }
 }
